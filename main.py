2d46b2fa0969010f17fb6e64f72656e9a4db59d0
Denis Smajlović <deni@deni.dk>
2022-07-02 16:25:55 -0700

    Implement device_remove function

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import textwrap
 import json
+import copy
 
 def sign(devices):
-	return "NZJr7Gs7YWPL3uZw/5GgEw=="
+	return('NZJr7Gs7YWPL3uZw/5GgEw==')
 
 def chain_create():
 	# Generate device key pair
-	key_private = ""
-	key_public = "rPN7a3A7tLTfcCUTg0gTgQ=="
+	key_private = ''
+	key_public = 'rPN7a3A7tLTfcCUTg0gTgQ=='
 
 	# Have the user pick an identifier for the device
 	device_name = input("Enter a name for this device: ")
@@ -22,10 +23,10 @@ def chain_create():
 
 	# Create an object to cotain this
 	object = {
-		"devices": devices,
-		"supersedes": None,
-		"signee": key_public,
-		"signature": signature # "NZJr7Gs7YWPL3uZw/5GgEw=="
+		'devices': devices,
+		'supersedes': None,
+		'signee': key_public,
+		'signature': signature # "NZJr7Gs7YWPL3uZw/5GgEw=="
 	}
 
 	# Put it in a list, creating the first link of the chain
@@ -48,14 +49,33 @@ def chain_load(file_path):
 def json_serialize(object):
 	return(json.dumps(object, indent='\t'))
 
-def add_device(chain):
+def device_add(chain):
 	device_public_key = input("Enter the public key of the device that you wish to add: ")
 	device_name = input("What do you wish to call this device? ")
 
-	chain[-1]["devices"][device_public_key] = device_name
+	link = copy.deepcopy(chain[-1])
+	link['devices'][device_public_key] = device_name
+
+	chain.append(link)
 
 	return(chain)
 
+def device_remove(chain):
+	link = copy.deepcopy(chain[-1])
+
+	options = []
+
+	for device in link['devices']:
+		options.append(str(device))
+		print("[{}] {}".format(len(options), link['devices'][device]))
+
+	choice = input("Which key do you wish to delete? ")
+
+	del link['devices'][options[int(choice) - 1]]
+
+	chain.append(link)
+
+	return(chain)
 
 chain = None
 saved = True
@@ -92,9 +112,9 @@ while True:
 	elif choice == "3":
 		pass
 	elif choice == "4":
-		chain = add_device(chain)
+		chain = device_add(chain)
 	elif choice == "5":
-		pass
+		chain = device_remove(chain)
 	else:
 		print("Please make a choice.")
 		choose()
