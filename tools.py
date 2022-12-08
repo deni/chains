7aba69375f4329396b6971250c5de3ffdd6dde4e
Denis Smajlović <deni@deni.dk>
2022-12-08 14:53:23 +0100

    Add tools.py

---
+++ tools.py
@@ -0,0 +1,21 @@
+import base64
+import json
+
+
+def base64_string(bytes):
+    bytes_base64 = base64.b64encode(bytes)
+    string = bytes_base64.decode('utf-8')
+
+    return string
+
+
+def json_serialize_canonical(object):
+    serialized = json.dumps(object,
+        sort_keys = True,
+        indent = None,
+        separators = (',', ':'),
+        ensure_ascii = False,
+        allow_nan = False,
+    )
+
+	return serialized
