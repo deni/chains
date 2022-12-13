b1d2a691b9a12b9e499f1872af8cf8935ea6f208
Denis Smajlović <deni@deni.dk>
2022-12-13 18:16:22 +0100

    Add chain_clean and chain_integrity

--- chain_tools.py
+++ chain_tools.py
@@ -1,5 +1,7 @@
 import base64
 import json
+import time
+from cryptography.hazmat.primitives import hashes
 
 
 def base64_string(bytes):
@@ -7,6 +9,72 @@ def base64_string(bytes):
 	string = bytes_base64.decode('utf-8')
 
 	return string
+
+
+def log(text):
+    text_base = '{time} | {text}'
+    print(text_base.format(
+        time = time.strftime( "%H:%M:%S", time.localtime() ),
+        text = text,
+    ))
+
+
+def is_base64_string(input):
+	try:
+		decoded = base64.b64decode(input)
+		encoded = base64.b64encode(decoded)
+	except:
+		return False
+
+	output = encoded.decode('utf-8')
+
+	return input == output
+
+
+def pretty_log(object):
+    serialized_pretty = json.dumps(object, indent='\t')
+    log(serialized_pretty)
+
+
+def chain_clean(untrusted_chain):
+	def devices_clean(untrusted_devices):
+		devices = {}
+		for device_key, device_name in untrusted_devices.items():
+			if is_base64_string(device_key):
+				devices[device_key] = device_name
+
+		return devices
+
+	chain = []
+
+	for untrusted_chainlink in untrusted_chain:
+		untrusted_devices = untrusted_chainlink['devices']
+		devices = devices_clean(untrusted_devices)
+
+		untrusted_supersedes = untrusted_chainlink['supersedes']
+		untrusted_signee = untrusted_chainlink['signee']
+		untrusted_signature = untrusted_chainlink['signature']
+
+		if is_base64_string(untrusted_supersedes) or untrusted_supersedes is None:
+			supersedes = untrusted_supersedes
+		else: return False
+		if is_base64_string(untrusted_signee) or untrusted_signee is None:
+			signee = untrusted_signee
+		else: return False
+		if is_base64_string(untrusted_signature) or untrusted_signature is None:
+			signature = untrusted_signature
+		else: return False
+
+		chainlink = {
+			'devices': devices,
+			'supersedes': supersedes,
+			'signee': signee,
+			'signature': signature,
+		}
+
+		chain.append(chainlink)
+
+	return chain
 
 
 def json_serialize_canonical(object):
@@ -19,3 +87,55 @@ def json_serialize_canonical(object):
 	)
 
 	return serialized
+
+
+def crypto_hash(bytes):
+	digest = hashes.Hash(hashes.SHA256())
+	digest.update(bytes)
+	hash = digest.finalize()
+
+	return(hash)
+
+
+def crypto_hash_human(string):
+	bytes = string.encode('utf-8')
+	hashed = crypto_hash(bytes)
+	hashed_base64 = base64.b64encode(hashed).decode('utf-8')
+
+	return(hashed_base64)
+
+
+def chain_integrity(chain):
+	def link_integrity(link, link_hash_claim):
+		link_json_canonical = json_serialize_canonical(link)
+		link_hash = crypto_hash_human(link_json_canonical)
+
+		if link_hash == link_hash_claim:
+			return True
+		else: return False
+
+
+	if chain[0]['supersedes'] is not None:
+		return False
+
+	chain_length = len(chain)
+
+	chainlinks_verified = []
+	if chain_length > 1:
+		for i in range(1, chain_length):
+			chainlinks_verified.append(False)
+
+	# Iterate and skip the last element
+	for link_index, chainlink in enumerate(chain[0:-1]):
+		# Get hash from the next chainlink
+		link_hash_claim = chain[link_index + 1]['supersedes']
+
+		if link_integrity(chainlink, link_hash_claim):
+			chainlinks_verified[link_index] = True
+
+	# Check if all the links are verified
+	for verified in chainlinks_verified:
+		if not verified: break
+	else: return True
+
+	return False
--- server.py
+++ server.py
@@ -1,14 +1,43 @@
 import os
 import chain_tools
+import base64
+import json
+# TMP; weird fix for command line input limit
+import readline
 
-
-def challenge_generate():
+def challenge_generate_bytes():
 	challenge_bytes = os.urandom(32)
-	challenge_string = chain_tools.base64_string(challenge_bytes)
 
-	return challenge_string
+	return challenge_bytes
 
 
-challenge_server = challenge_generate()
+def verify(chain, publickey, challenge, signature):
+	return False
 
-print("Server's challenge part: " + challenge_server)
+
+# Server challenge
+challenge_server_bytes = challenge_generate_bytes()
+challenge_server_string = chain_tools.base64_string(challenge_server_bytes)
+chain_tools.log("Server's challenge part: " + challenge_server_string)
+
+# Client challenge
+untrusted_challenge_client_string = input("Client's challenge part: ")
+challenge_client_bytes = base64.b64decode(untrusted_challenge_client_string)
+
+# Mutual challenge
+challenge_concat_bytes = challenge_server_bytes + challenge_client_bytes
+challenge_mutual_bytes = chain_tools.crypto_hash(challenge_concat_bytes)
+challenge_mutual_string = chain_tools.base64_string(challenge_mutual_bytes)
+chain_tools.log("Mutually agreed challenge: " + challenge_mutual_string)
+
+# Chain
+untrusted_chain_json = input("Please enter chain: ")
+untrusted_chain = json.loads(untrusted_chain_json)
+chain_unchecked = chain_tools.chain_clean(untrusted_chain)
+
+if chain_tools.chain_integrity(chain_unchecked):
+	chain = chain_unchecked
+	chain_tools.log("Chain passed integrity check!! \o/")
+else:
+	chain_tools.log("Chain did not pass integrity check.")
+	exit()
